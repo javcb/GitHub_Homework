51424c3c9ef40d7c30017a512df475ae459de6da
Javier Benitez <javier.c.benitez@gmail.com>
2018-12-12 15:10:51 -0500

    sync

--- 8_Homeowrk_SQL/homework.sql
+++ 8_Homeowrk_SQL/homework.sql
@@ -24,14 +24,26 @@ select country_id, country from country where country in ("Afghanistan", "Bangla
 -- 4d
 
 -- 5a
-SHOW CREATE TABLE address
+SHOW CREATE TABLE address;
 
 -- 6a
+select staff.first_name, staff.last_name, address.address from staff 
+inner join address on staff.address_id = address.address_id;
 
 -- 6b
+select staff.first_name, staff.last_name, sum(payment.amount) as total_payment from staff 
+inner join payment on staff.staff_id = payment.staff_id;
+
 -- 6c
+select film.title, count(film_actor.actor_id) as actor_count from film 
+inner join film_actor on film.film_id = film_actor.film_id;
+
 -- 6d
+select count(inventory_id) as inventory_count from inventory where film_id = 439;
+
 -- 6e
+select staff.first_name, staff.last_name, sum(payment.amount) as total_payment from staff 
+inner join payment on staff.staff_id = payment.staff_id;
 
 -- 7a
 -- 7b
