20f89558e023fb496e8056b550977bd6256d82bc
Javier <javier.c.benitez@gmail.com>
2018-12-15 15:58:24 -0500

    sync

--- 8_Homeowrk_SQL/homework.sql
+++ 8_Homeowrk_SQL/homework.sql
@@ -46,12 +46,37 @@ select customer.last_name, sum(payment.amount) as total_payment from customer
 inner join payment on customer.customer_id = payment.customer_id;
 
 -- 7a
-select title from film where title like ";
+-- select title from film where title like "k%" or title like "q%" and language_id = 1;
+-- select * from language;
+
+select title from film where title like "k%" or title like "q%" and language_id IN
+(select language_id from language where name IN ('English'));
 
 -- 7b
+select first_name, last_name from actor where actor_id IN
+(select actor_id from film_actor where film_id IN 
+(select film_id from film where title IN ('Alone Trip')));
+
 -- 7c
+select first_name, last_name, email from customer where address_id IN
+(select address_id from address where city_id IN
+(select city_id from city where country_id IN 
+(select country_id from country where country IN ('Canada'))));
+
 -- 7d
+-- select * from category;
+
+select title from film where film_id IN
+(select film_id from film_category where category_id IN
+(select category_id from category where name IN ('Family')));
+
 -- 7e
+select inventory.film_id, count(rental.rental_id) as rental_count from inventory
+inner join rental on inventory.inventory_id = rental.inventory_id;
+
+select customer.last_name, sum(payment.amount) as total_payment from customer
+inner join payment on customer.customer_id = payment.customer_id;
+
 -- 7f
 -- 7g
 -- 7h
